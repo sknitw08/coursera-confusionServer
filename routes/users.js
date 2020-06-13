1a53a5af5fe401004d3dc58ce9590236086d2ae8
Sanjay Kumar <sk.nitw08@gmail.com>
2020-06-14 01:13:04 +0530

    Assignment 4

--- routes/users.js
+++ routes/users.js
@@ -9,8 +9,16 @@ const authenticate = require('../authenticate');
 router.use(bodyParser.json());
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/', authenticate.verifyUser, authenticate.verifyAdmin, function(req, res, next) {
+  User.find({})
+    .then((users) => {
+        res.statusCode = 200;
+        res.setHeader("Content-Type", "application/json");
+        res.json(users);
+      },
+      err => next(err)
+    )
+    .catch(err => next(err));
 });
 
 router.post('/signup', (req, res, next) => {
@@ -51,7 +59,7 @@ router.post('/login', passport.authenticate('local'), (req, res) => {
   res.json({success: true, token: token, status: 'You are successfully logged in!'});
 });
 
-router.get('/logout', (req, res) => {
+router.get('/logout', (req, res, next) => {
   if (req.session) {
     req.session.destroy();
     res.clearCookie('session-id');
